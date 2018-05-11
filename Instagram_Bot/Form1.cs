b092f643bb832597cd5cf9781c8d804dab8986a4
Stuart Green <stuart.green@cmuk.org>
2018-05-11 21:10:40 +0100

    Added a service loop, now the bot never stops. There is as 5 minutes delay between loops. Added hashtags.txt, so other can contribute hashtags easily.

--- Instagram_Bot/Form1.cs
+++ Instagram_Bot/Form1.cs
@@ -2,6 +2,8 @@
 using System.Windows.Forms;
 using OpenQA.Selenium.Chrome;
 using System.Collections.Generic;
+using System.IO;
+
 namespace Instagram_Bot
 {
     public partial class Form1 : Form
@@ -16,6 +18,7 @@ namespace Instagram_Bot
         {
             var botCore = new bot_core(textBoxUsername.Text.Trim(), textBoxPassword.Text.Trim());
         }
+
     }
 
     public class bot_core
@@ -32,55 +35,76 @@ namespace Instagram_Bot
             System.Threading.Thread.Sleep(3 * 1000);// wait 5 seconds for page to change
             #endregion
 
-            #region perform a random search from the list of search terms
             List<string> thingsToSearch = new List<string>()
             {
                 "summer", "web developer", "weekend", "friday", "netflix",
                 "chill", "bournemouth", "poole", "dorset", "vsafety"
             };
 
-            var mySearch = thingsToSearch[new Random().Next(0, thingsToSearch.Count - 1)];
+            // load more tags from the hashtags.txt file, not concerned about duplication.
+            thingsToSearch.AddRange(File.ReadLines("hashtags.txt"));
 
-            // just navigate to search
-            driver.Navigate().GoToUrl($"https://www.instagram.com/explore/tags/{mySearch}");
-            System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
+            /* MAIN LOOP */
 
-            // save results
-            List<string> postsToLike = new List<string>();
-            foreach (var link in driver.FindElementsByTagName("a"))
-            {
-                if (link.GetAttribute("href").ToUpper().Contains($"TAGGED={mySearch.ToUpper()}"))
-                {
-                    postsToLike.Add(link.GetAttribute("href"));
-                }
-            }
 
-            // load results in turn and like/follow them
-            foreach (string glink in postsToLike)
+            // loop forever, performing a new search and them following everyone.
+            while (true)
             {
 
-                if (glink.Contains("https://www.instagram.com/"))
-                {
-                    driver.Navigate().GoToUrl(glink);
-                }
-                else
-                {
-                    driver.Navigate().GoToUrl("https://www.instagram.com/" + glink);
-                }
+                var mySearch = thingsToSearch[new Random().Next(0, thingsToSearch.Count - 1)];
 
+                // just navigate to search
+                driver.Navigate().GoToUrl($"https://www.instagram.com/explore/tags/{mySearch}");
                 System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
 
-                foreach (var button in driver.FindElementsByTagName("button"))
+                // save results
+                List<string> postsToLike = new List<string>();
+                foreach (var link in driver.FindElementsByTagName("a"))
                 {
-                    if (button.Text.ToUpper().Contains("FOLLOW"))
+                    if (link.GetAttribute("href").ToUpper().Contains($"TAGGED={mySearch.ToUpper()}"))
                     {
-                        button.Click();
-                        System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
-                        continue;
+                        postsToLike.Add(link.GetAttribute("href"));
                     }
                 }
+
+                // load results in turn and like/follow them
+                foreach (string glink in postsToLike)
+                {
+
+                    if (glink.Contains("https://www.instagram.com/"))
+                    {
+                        driver.Navigate().GoToUrl(glink);
+                    }
+                    else
+                    {
+                        driver.Navigate().GoToUrl("https://www.instagram.com/" + glink);
+                    }
+
+                    System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
+
+                    foreach (var button in driver.FindElementsByTagName("button"))
+                    {
+                        if (button.Text.ToUpper().Contains("FOLLOW"))
+                        {
+                            button.Click();
+                            System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
+                            continue;
+                        }
+                    }
+                }
+
+
+                // Return to users profile page so they can see their stats while we wait for next search to start
+                driver.Navigate().GoToUrl($"https://www.instagram.com/{username}");
+                System.Threading.Thread.Sleep(5 * 60000);// wait 5 minutes between each search so not to be spammy
             }
-            #endregion
+
+
+
+            /* end of MAIN LOOP */
+
+
+
         }
     }
 
