a543a84ae23c688cd4b77cd356b88beec599c549
Stuart Green <stuart.green@cmuk.org>
2018-05-11 20:29:54 +0100

    First working commit (it auto follows)

--- Instagram_Bot/Form1.Designer.cs
+++ Instagram_Bot/Form1.Designer.cs
@@ -28,81 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStartBot = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(760, 66);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Mode";
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(53, 28);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "(1) Build Followers";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(324, 28);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "(2) Get Likes";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(584, 28);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(115, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "(3) Fully Automated";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.webBrowser1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 84);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(760, 444);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Browser (see what\'s happening)";
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 82);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(770, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -112,38 +54,63 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(50, 17);
             this.toolStripStatusLabel1.Text = "Loading";
             // 
-            // webBrowser1
+            // textBoxUsername
             // 
-            this.webBrowser1.AllowWebBrowserDrop = false;
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
-            this.webBrowser1.Location = new System.Drawing.Point(3, 16);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(754, 425);
-            this.webBrowser1.TabIndex = 0;
-            this.webBrowser1.Url = new System.Uri("https://www.instagram.com/", System.UriKind.Absolute);
-            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            this.textBoxUsername.Location = new System.Drawing.Point(176, 35);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(121, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(435, 35);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(121, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Instagram Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Instagram Password:";
+            // 
+            // buttonStartBot
+            // 
+            this.buttonStartBot.Location = new System.Drawing.Point(596, 33);
+            this.buttonStartBot.Name = "buttonStartBot";
+            this.buttonStartBot.Size = new System.Drawing.Size(109, 23);
+            this.buttonStartBot.TabIndex = 2;
+            this.buttonStartBot.Text = "Start Bot";
+            this.buttonStartBot.UseVisualStyleBackColor = true;
+            this.buttonStartBot.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(770, 104);
+            this.Controls.Add(this.buttonStartBot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(800, 600);
-            this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Instagram Bot - (Open Source) - By Stuart Green";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -152,15 +119,13 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        protected System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStartBot;
     }
 }
 
--- Instagram_Bot/Form1.cs
+++ Instagram_Bot/Form1.cs
@@ -1,20 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
 namespace Instagram_Bot
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var botCore = new bot_core(textBoxUsername.Text.Trim(), textBoxPassword.Text.Trim());
+        }
     }
+
+    public class bot_core
+    {
+        ChromeDriver driver = new ChromeDriver();
+        public bot_core(string username, string password)
+        {
+
+            #region Login to Instagram
+            driver.Navigate().GoToUrl("https://www.instagram.com/accounts/login/");
+            driver.FindElementByName("username").SendKeys(username);
+            driver.FindElementByName("password").SendKeys(password);
+            driver.FindElementByTagName("form").Submit();
+            System.Threading.Thread.Sleep(3 * 1000);// wait 5 seconds for page to change
+            #endregion
+
+            #region perform a random search from the list of search terms
+            List<string> thingsToSearch = new List<string>()
+            {
+                "summer", "web developer", "weekend", "friday", "netflix",
+                "chill", "bournemouth", "poole", "dorset", "vsafety"
+            };
+
+            var mySearch = thingsToSearch[new Random().Next(0, thingsToSearch.Count - 1)];
+
+            // just navigate to search
+            driver.Navigate().GoToUrl($"https://www.instagram.com/explore/tags/{mySearch}");
+            System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
+
+            // save results
+            List<string> postsToLike = new List<string>();
+            foreach (var link in driver.FindElementsByTagName("a"))
+            {
+                if (link.GetAttribute("href").ToUpper().Contains($"TAGGED={mySearch.ToUpper()}"))
+                {
+                    postsToLike.Add(link.GetAttribute("href"));
+                }
+            }
+
+            // load results in turn and like/follow them
+            foreach (string glink in postsToLike)
+            {
+
+                if (glink.Contains("https://www.instagram.com/"))
+                {
+                    driver.Navigate().GoToUrl(glink);
+                }
+                else
+                {
+                    driver.Navigate().GoToUrl("https://www.instagram.com/" + glink);
+                }
+
+                System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
+
+                foreach (var button in driver.FindElementsByTagName("button"))
+                {
+                    if (button.Text.ToUpper().Contains("FOLLOW"))
+                    {
+                        button.Click();
+                        System.Threading.Thread.Sleep(3 * 1000);// wait for page to change
+                        continue;
+                    }
+                }
+            }
+            #endregion
+        }
+    }
+
 }
