5e080cc81bb09ecf8177cb324e15785a01720857
Stuart Green <stuart.green@cmuk.org>
2018-05-20 09:41:39 +0100

    ..

--- Instagram_Bot/Properties/AssemblyInfo.cs
+++ Instagram_Bot/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Instagram_Bot")]
+[assembly: AssemblyTitle("Instagram_Bot_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Instagram_Bot")]
+[assembly: AssemblyProduct("Instagram_Bot_1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
