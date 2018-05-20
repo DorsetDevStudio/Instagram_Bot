d07aca2c4c9ce2ee52c1958a736dba7adb3a36c1
Stuart Green <stuart.green@cmuk.org>
2018-05-20 21:40:29 +0100

    test db

--- Instagram_Bot/Classes/c_bot_core.cs
+++ Instagram_Bot/Classes/c_bot_core.cs
@@ -217,13 +217,13 @@ namespace Instagram_Bot
                 }
                 catch (InvalidOperationException ee)
                 {                  
-                    if (enableVoices) C_voice_core.speak($"SQLite invalid operation error {ee.InnerException.ToString()}",true);
-                    MessageBox.Show($"SQLite invalid operation error {ee.InnerException.ToString()}");
+                    if (enableVoices) C_voice_core.speak($"SQLite invalid operation error {ee.InnerException.Message}",true);
+                    MessageBox.Show($"SQLite invalid operation error {ee.InnerException.Message}");
                 }
                 catch (Exception ee)
                 {
-                    if (enableVoices) C_voice_core.speak($"SQLite error {ee.InnerException.ToString()}",true);
-                    MessageBox.Show($"SQLite error {ee.InnerException.ToString()}");
+                    if (enableVoices) C_voice_core.speak($"SQLite error {ee.InnerException.Message}",true);
+                    MessageBox.Show($"SQLite error {ee.InnerException.Message}");
                 }
               
                 DateTime commentingBannedUntil = DateTime.Now;
